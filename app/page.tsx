e5383723250ca0f33504a5936b5a1f8090a1fb2f
aginamena <aginamena5@gmail.com>
2025-07-14 13:30:02 -0400

    removed grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Grid, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import Link from "next/link";
 
 export default function Home() {
